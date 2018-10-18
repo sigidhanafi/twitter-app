7b7e5ea88b630effd623d8dc1c7a5c16fcc2e9f6
sigidhanafi <sigidhanafi@gmail.com>
2018-10-18 13:32:30 +0700

    useing component from Podfile

---
+++ Twitter/Controller/HomeDatasourceController.swift
@@ -0,0 +1,26 @@
+//
+//  HomeDatasourceController.swift
+//  Twitter
+//
+//  Created by Sigit Hanafi on 4/25/18.
+//  Copyright © 2018 Sigit Hanafi. All rights reserved.
+//
+
+import LBTAComponents
+
+class HomeDatasourceController: DatasourceController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let homeDatasource = HomeDatasource()
+        self.datasource = homeDatasource
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 100)
+    }
+}
---
+++ Twitter/View/Cells.swift
@@ -0,0 +1,9 @@
+//
+//  Cells.swift
+//  Twitter
+//
+//  Created by Sigit Hanafi on 18/10/18.
+//  Copyright © 2018 Sigit Hanafi. All rights reserved.
+//
+
+import Foundation
